6a62aca267eebfdeda102b2d196e0446213d5804
bengoodey <bgoodey@frontier.com>
2025-02-27 15:22:19 -0700

    added the ms.h file and fixed the code. Everything outputs as string or float

--- PocketQube2025/src/sensors.cpp
+++ PocketQube2025/src/sensors.cpp
@@ -1,6 +1,11 @@
 #include <Adafruit_MMC56x3.h>
 #include <LSM6DSOX.h>
+#include <MS5611.h>
 //#include <cmath> //may be needed, not super sure
+#define MAG_ALPHA 0.8
+#define DEG_ALPHA 0.8
+#define ACC_ALPHA 0.8
+#define MS_ALPHA 0.8
 
 
 //********************************************************************************************************** */
@@ -9,92 +14,58 @@ class MMCModule {
     private:
     bool magflag = false;
     Adafruit_MMC5603 mag = Adafruit_MMC5603(12345);
-    double magnet_data[3];
-    double degrees;
+    float magnet_data[3];
+    float degrees;
 
-    void collect_mag(){
-        double temp[3];
+    void collect_mag(bool init = false){
         sensors_event_t event;
         mag.getEvent(&event);
-        temp[0] = event.magnetic.x;
-        temp[1] = event.magnetic.y;
-        temp[2] = event.magnetic.z;
-    }
-    // double collect_mag(){ //may need alpha filter
-    //     double result[3];
-    //     sensors_event_t event;
-    //     mag.getEvent(&event);
-    //     result[0] = event.magnetic.x;
-    //     result[1] = event.magnetic.y;
-    //     result[2] = event.magnetic.z;
-    //     return result;
-    // }
-
-    // double collect_deg(){
-    //     sensors_event_t event;
-    //     mag.getEvent(&event);
-    //     return atan2(event.magnetic.y, event.magnetic.x) * 180 / 3.14159;
-    // }
-    //collects N samples and averages the middle ones
-    String collect_N_mag(uint8_t N){// N >= 3
-        if (N < 3) return "N too small";  
-
-        sensors_event_t mag_event[N];  
-        float magnitudes[N];  
-
-        // Collect data and compute magnitudes
-        for (int i = 0; i < N; i++) {
-            mag.getEvent(&mag_event[i]);
-            float x = mag_event[i].magnetic.x;
-            float y = mag_event[i].magnetic.y;
-            float z = mag_event[i].magnetic.z;
-            magnitudes[i] = sqrt(x * x + y * y + z * z);
-            delay(5);
+        if (init){
+            magnet_data[0] = event.magnetic.x;
+            magnet_data[1] = event.magnetic.y;
+            magnet_data[2] = event.magnetic.z;
         }
-
-        // Sort based on precomputed magnitudes
-        std::sort(mag_event, mag_event + N, [&](const sensors_event_t &a, const sensors_event_t &b) {
-            return magnitudes[&a - mag_event] < magnitudes[&b - mag_event];
-        }); 
-
-        // Compute the average excluding the lowest and highest values  
-        float avg_x = 0, avg_y = 0, avg_z = 0;  
-        for (int i = 1; i < N - 1; i++) {  
-            avg_x += mag_event[i].magnetic.x;  
-            avg_y += mag_event[i].magnetic.y;  
-            avg_z += mag_event[i].magnetic.z;  
-        }  
-
-        avg_x /= (N - 2);  
-        avg_y /= (N - 2);  
-        avg_z /= (N - 2);  
-
-        return "X: " + String(avg_x, 3) + " |Y: " + String(avg_y, 3) + " |Z: " + String(avg_z, 3);  
+        else{
+            float temp[3];
+            temp[0] = event.magnetic.x * MAG_ALPHA + magnet_data[0] * (1 - MAG_ALPHA);
+            temp[1] = event.magnetic.y * MAG_ALPHA + magnet_data[1] * (1 - MAG_ALPHA);
+            temp[2] = event.magnetic.z * MAG_ALPHA + magnet_data[2] * (1 - MAG_ALPHA);
+            magnet_data[0] = temp[0];
+            magnet_data[1] = temp[1];
+            magnet_data[2] = temp[2];
+        }
     }
 
-    String collect_N_deg(uint8_t N){
-        if(magflag) return "MMC5603 not initialized";
-
-        sensors_event_t deg_event[N];
-        float degrees[N];
-
-        //for loop for getting the degrees
-        for (int i = 0; i < N; i++) {
-            mag.getEvent(&deg_event[i]);
-            degrees[i] = atan2(deg_event[i].magnetic.y, deg_event[i].magnetic.x) * 180 / 3.14159;
-            delay(5);
+    void collect_deg(bool init = false){
+        sensors_event_t event;
+        mag.getEvent(&event);
+        if (init){
+            degrees = atan2(event.magnetic.y, event.magnetic.x) * 180 / 3.14159;
         }
-
-        //sort
-        std::sort(degrees, degrees + N);
-
-        //average
-        float avg_degree = 0;
-        for (int i = 1; i < N - 1; i++) {
-            avg_degree += degrees[i];
+        else{
+            degrees = degrees * DEG_ALPHA + atan2(event.magnetic.y, event.magnetic.x) * 180 / 3.14159 * (1 - DEG_ALPHA);
         }
-        avg_degree /= (N - 2);
-        return String(avg_degree, 2) + "°";
+    }
+
+    void collect_magnet(bool init = false){ //has everything in one call
+        sensors_event_t event;
+        mag.getEvent(&event);
+        if (init){
+            magnet_data[0] = event.magnetic.x;
+            magnet_data[1] = event.magnetic.y;
+            magnet_data[2] = event.magnetic.z;
+            degrees = atan2(event.magnetic.y, event.magnetic.x) * 180 / 3.14159;
+        }
+        else{
+            float temp[3];
+            temp[0] = event.magnetic.x * MAG_ALPHA + magnet_data[0] * (1 - MAG_ALPHA);
+            temp[1] = event.magnetic.y * MAG_ALPHA + magnet_data[1] * (1 - MAG_ALPHA);
+            temp[2] = event.magnetic.z * MAG_ALPHA + magnet_data[2] * (1 - MAG_ALPHA);
+            magnet_data[0] = temp[0];
+            magnet_data[1] = temp[1];
+            magnet_data[2] = temp[2];
+            degrees = degrees * DEG_ALPHA + atan2(event.magnetic.y, event.magnetic.x) * 180 / 3.14159 * (1 - DEG_ALPHA);
+        }
     }
 
     public:
@@ -106,28 +77,27 @@ class MMCModule {
         }
         else {
             magflag = false;
+            collect_deg(true);
+            collect_mag(true);
+            //collect_magnet(true);
             return "MMC5603 Initialized";
         }
     }
 
-    double get_data(){ //x, y, z, degrees
+    float get_data(){ //x, y, z, degrees
         if (magflag) return -1;
-        double result[4];
-        double temp[3] = collect_mag();
-        result[0] = temp[0];
-        result[1] = temp[1];
-        result[2] = temp[2];
-        result[3] = collect_deg();
+        float result[4];
+        result[0] = magnet_data[0];
+        result[1] = magnet_data[1];
+        result[2] = magnet_data[2];
+        result[3] = degrees;
         return result;
     }
-    String tick_mag(uint8_t N){ // N is the number of samples to be averaged, at least 3
-        if (magflag) return "MMC5603 not initialized";
-        return "Mag Sensor (uT) " + collect_N_mag(N);
-    }
-
-    String tick_deg(uint8_t N){ // N is the number of samples to be averaged, at least 3
-        if (magflag) return "MMC5603 not initialized";
-        return "Degrees: " + collect_N_deg(N);
+    void tick(){//select the right one here
+        if (magflag) return;
+        collect_deg();
+        collect_mag();
+        //collect_magnet();
     }
 
 };
@@ -138,147 +108,92 @@ class LMSModule{
     private:
 
     bool accflag = false;
-    
-    double collect_acc(){
-        double result[3];
-        if (!IMU.accelerationAvailable()) return "LSM6DSOX: Acceleration not available";
-        IMU.readAcceleration(result[0], result[1], result[2]);
-        return result;
+    float accel_data[3];
+    float gyro_data[3];
+    float temp_data;
+    //does this need an IMU init? the arduino files don't 
+
+    void collect_acc(bool init = false){
+        if (!IMU.accelerationAvailable()) accel_data = 0;
+        if (init){
+            IMU.readAcceleration(accel_data[0], accel_data[1], accel_data[2]);
+        }
+        else{
+            float temp[3];
+            IMU.readAcceleration(temp[0], temp[1], temp[2]);
+            accel_data[0] = temp[0] * ACC_ALPHA + accel_data[0] * (1 - ACC_ALPHA);
+            accel_data[1] = temp[1] * ACC_ALPHA + accel_data[1] * (1 - ACC_ALPHA);
+            accel_data[2] = temp[2] * ACC_ALPHA + accel_data[2] * (1 - ACC_ALPHA);
+        }
     }
 
-    double collect_gyro(){
-        double result[3];
-        if (!IMU.gyroscopeAvailable()) return "LSM6DSOX: Gyroscope not available";
-        IMU.readGyroscope(result[0], result[1], result[2]);
-        return result;
+    void collect_gyro(bool init = false){
+        if (!IMU.gyroscopeAvailable()) gyro_data = 0;
+        if (init){
+            IMU.readGyroscope(gyro_data[0], gyro_data[1], gyro_data[2]);
+        }
+        else{
+            float temp[3];
+            IMU.readGyroscope(temp[0], temp[1], temp[2]);
+            gyro_data[0] = temp[0] * ACC_ALPHA + gyro_data[0] * (1 - ACC_ALPHA);
+            gyro_data[1] = temp[1] * ACC_ALPHA + gyro_data[1] * (1 - ACC_ALPHA);
+            gyro_data[2] = temp[2] * ACC_ALPHA + gyro_data[2] * (1 - ACC_ALPHA);
+        }
     }
 
-    double collect_temp(){ //Redundant?
-        double result;
-        if (!IMU.temperatureAvailable()) return "LSM6DSOX: Temperature not available";
-        bool readSuccess = IMU.readTemperatureFloat(result);
-        if(!readSuccess) return "LSM6DSOX: Temperature not available";
-        return double(result);
+    void collect_temp(bool init = false){
+        if (!IMU.temperatureAvailable()) temp_data = 0;
+        if (init){
+            IMU.readTemperatureFloat(temp_data);
+            // if(!readSuccess) return "LSM6DSOX: Temperature not available";
+        }
+        else{
+            float temp;
+            IMU.readTemperatureFloat(temp);
+            // if(!readSuccess) return "LSM6DSOX: Temperature not available";
+            temp_data = temp * ACC_ALPHA + temp_data * (1 - ACC_ALPHA);
+        }
     }
 
-    String collect_N_acc(uint8_t N){
-        if (accflag) return "LSM6DSOX not initialized";
-        if (N < 3) return "N too small";
-
-        float acc_data[N][3];  // Store x, y, z
-        float magnitudes[N];
-
-        // Collect data and compute magnitudes
-        for (int i = 0; i < N; i++) {
-            while (!IMU.accelerationAvailable());
-            IMU.readAcceleration(acc_data[i][0], acc_data[i][1], acc_data[i][2]);
-
-            float x = acc_data[i][0], y = acc_data[i][1], z = acc_data[i][2];
-            magnitudes[i] = sqrt(x * x + y * y + z * z);
-
-            delay(5);
+    void collect_all(bool init = false){
+        if (init){
+            IMU.readAcceleration(accel_data[0], accel_data[1], accel_data[2]);
+            IMU.readGyroscope(gyro_data[0], gyro_data[1], gyro_data[2]);
+            IMU.readTemperatureFloat(temp_data);
         }
+        else{
+            float temp[3];
+            IMU.readAcceleration(temp[0], temp[1], temp[2]);
+            accel_data[0] = temp[0] * ACC_ALPHA + accel_data[0] * (1 - ACC_ALPHA);
+            accel_data[1] = temp[1] * ACC_ALPHA + accel_data[1] * (1 - ACC_ALPHA);
+            accel_data[2] = temp[2] * ACC_ALPHA + accel_data[2] * (1 - ACC_ALPHA);
+            temp = 0;
+            IMU.readGyroscope(temp[0], temp[1], temp[2]);
+            gyro_data[0] = temp[0] * ACC_ALPHA + gyro_data[0] * (1 - ACC_ALPHA);
+            gyro_data[1] = temp[1] * ACC_ALPHA + gyro_data[1] * (1 - ACC_ALPHA);
+            gyro_data[2] = temp[2] * ACC_ALPHA + gyro_data[2] * (1 - ACC_ALPHA);
 
-        //these next few lines would not compile so I removed them until we can find a better solution.
+            float temp_t;
+            IMU.readTemperatureFloat(temp_t);
+            // if(!readSuccess) return "LSM6DSOX: Temperature not available";
+            temp_data = temp_t * ACC_ALPHA + temp_data * (1 - ACC_ALPHA);
 
-        // Sort indices based on magnitude
-        /*std::sort(acc_data, acc_data + N, [&](const float a[3], const float b[3]) {
-            return magnitudes[&a - acc_data] < magnitudes[&b - acc_data];
-        });*/
-
-        // Compute the average excluding the lowest and highest values
-        float avg_x = 0, avg_y = 0, avg_z = 0;
-        for (int i = 1; i < N - 1; i++) {
-            avg_x += acc_data[i][0];
-            avg_y += acc_data[i][1];
-            avg_z += acc_data[i][2];
         }
-
-        avg_x /= (N - 2);
-        avg_y /= (N - 2);
-        avg_z /= (N - 2);
-
-        return "X: " + String(avg_x, 3) + " |Y: " + String(avg_y, 3) + " |Z: " + String(avg_z, 3);
-
-    }
-
-    String collect_N_gyro(uint8_t N){
-        if (accflag) return "LSM6DSOX not initialized";
-        if (N < 3) return "N too small";
-
-        float gyro_data[N][3];  // Store x, y, z
-        float magnitudes[N];
-
-        // Collect data and compute magnitudes
-        for (int i = 0; i < N; i++) {
-            if (!IMU.gyroscopeAvailable()) return "LSM6DSOX: Gyroscope not available";
-            IMU.readGyroscope(gyro_data[i][0], gyro_data[i][1], gyro_data[i][2]);
-
-            float x = gyro_data[i][0], y = gyro_data[i][1], z = gyro_data[i][2];
-            magnitudes[i] = sqrt(x * x + y * y + z * z);
-
-            delay(5);
-        }
-
-        //these next few lines would not compile so I removed them until we can find a better solution.
-
-        // Sort indices based on magnitude
-        /*std::sort(gyro_data, gyro_data + N, [&](const float a[3], const float b[3]) {
-            return magnitudes[&a - gyro_data] < magnitudes[&b - gyro_data];
-        });*/
-
-        // Compute the average excluding the lowest and highest values
-        float avg_x = 0, avg_y = 0, avg_z = 0;
-        for (int i = 1; i < N - 1; i++) {
-            avg_x += gyro_data[i][0];
-            avg_y += gyro_data[i][1];
-            avg_z += gyro_data[i][2];
-        }
-
-        avg_x /= (N - 2);
-        avg_y /= (N - 2);
-        avg_z /= (N - 2);
-
-        return "X: " + String(avg_x, 3) + " |Y: " + String(avg_y, 3) + " |Z: " + String(avg_z, 3);
-    }
-
-    String collect_N_temp(uint8_t N){
-        if (accflag) return "LSM6DSOX not initialized";
-        if (N < 3) return "N too small";
-
-        float temp_data[N];
-        for (int i = 0; i < N; i++) {
-            if (!IMU.temperatureAvailable()) return "LSM6DSOX: Temperature not available";
-            bool readSuccess = IMU.readTemperatureFloat(temp_data[i]);
-            if(!readSuccess) return "LSM6DSOX: Temperature not available";
-            delay(5);
-        }
-
-        std::sort(temp_data, temp_data + N);
-
-        float avg_temp = 0;
-        for (int i = 1; i < N - 1; i++) {
-            avg_temp += temp_data[i];
-        }
-        avg_temp /= (N - 2);
-
-        return String(avg_temp, 3);
-
     }
 
     public:
 
-    double get_data(){
-        double result[7];
-        double temp[3] = collect_acc();
+    float get_data(){
+        float result[7];
+        float temp[3] = accel_data();
         result[0] = temp[0];
         result[1] = temp[1];
         result[2] = temp[2];
-        temp = collect_gyro();
+        temp = gyro_data();
         result[3] = temp[0];
         result[4] = temp[1];
         result[5] = temp[2];
-        result[6] = collect_temp();
+        result[6] = temp_data;
         return result;
 
     }
@@ -291,30 +206,107 @@ class LMSModule{
         }
         else {
             accflag = false;
+            collect_acc(true);
+            collect_gyro(true);
+            collect_temp(true);
+            // collect_all(true);
             return "LSM6DSOX Initialized";
         }
     }
-
-    String tick_acc(uint8_t N){ // N is the number of samples to be averaged, at least 3
-        if (accflag) return "LSM6DSOX not initialized";
-        return "Accel (m/s^2) " + collect_N_acc(N);
-    }
-
-    String tick_gyro(uint8_t N){ // N is the number of samples to be averaged, at least 3
-        if (accflag) return "LSM6DSOX not initialized";
-        return "Gyro (rad/s) " + collect_N_gyro(N);
-    }
-
-
-    String tick_temp(uint8_t N){
-        if (accflag) return "LSM6DSOX not initialized";
-        return "Temperature (°C): " + collect_N_temp(N);
+    void tick(){
+        if (accflag) return;
+        collect_acc();
+        collect_gyro();
+        collect_temp();
+        // collect_all();
     }
 
 };
 
 
 //********************************************************************************************************** */
+class MS5611Module{
+    private:
+    bool msflag = false;
+    MS5611 ms = MS5611(0x77); //may need to remove the ms. The code technically doens't need it
+    float pressure;
+    float temperature;
+
+    void collect_pressure(bool init = false){
+        ms.read();
+        if (init){
+            pressure = ms.getPressurePascal();
+        }
+        else{
+            float temp;
+            temp = ms.getPressurePascal();
+            pressure = temp * MS_ALPHA + pressure * (1 - MS_ALPHA);
+        }
+    }
+    void collect_temp(bool init = false){
+        ms.read();
+        if (init){
+            temperature = ms.getTemperature();
+        }
+        else{
+            float temp;
+            temp = ms.getTemperature();
+            temperature = temp * MS_ALPHA + temperature * (1 - MS_ALPHA);
+        }
+    }
+
+    void collect_all(bool init = false){
+        ms.read();
+        if (init){
+            pressure = ms.getPressurePascal();
+            temperature = ms.getTemperature();
+        }
+        else{
+            float temp;
+            temp = ms.getPressurePascal();
+            pressure = temp * MS_ALPHA + pressure * (1 - MS_ALPHA);
+            temp = ms.getTemperature();
+            temperature = temp * MS_ALPHA + temperature * (1 - MS_ALPHA);
+        }
+    }
+
+
+
+    public:
+
+    String init_MS5611(){
+        if(!ms.begin()){
+            msflag = true;
+            return "MS5611 Failure";
+        }
+        else {
+            msflag = false;
+            ms.setOversampling(OSR_ULTRA_HIGH);// Can change to other settings
+            ms.reset
+            collect_pressure(true);
+            //collect_temp(true);
+            return "MS5611 Initialized";
+        }
+    }
+
+    float get_data(){
+        if (msflag) return -1;
+        float result[2];
+        result[0] = pressure;
+        result[1] = temperature;
+        return result;
+    }
+
+    void tick(){
+        if (msflag) return;
+        collect_pressure();
+        //collect_temp();
+    }
+
+
+
+};
+
 /*
 #include "MS5611.h"
 MS5611 MS5611(0x77);
@@ -329,10 +321,6 @@ MS5611.read()
 MS5611.getPressure();
 MS5611.getPressurePascal();
 MS5611.getTemperature();
-
-
-
-
 */
 
 /*
