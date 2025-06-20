a35769d65d21a89d138944ac7a6e022b1edba155
Swimnato <66497456+Swimnato@users.noreply.github.com>
2025-06-20 16:27:14 -0600

    Fixed Pin definitions

--- PocketQube2025/src/pinDefinitions.h
+++ PocketQube2025/src/pinDefinitions.h
@@ -1,11 +1,7 @@
 #ifndef PIN_DEFINITIONS_H
 #define PIN_DEFINITIONS_H
 
-<<<<<<< Updated upstream
-#define SD_CS 6
-#define CAM_CS 7
-#define RAD_CS 3
-=======
+
 
 #define RAD_CS 3
 
@@ -16,6 +12,5 @@
 #define SD_CS 6
 #define CAM_CS 7
 #endif
->>>>>>> Stashed changes
 
 #endif
